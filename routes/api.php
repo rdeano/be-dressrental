110e953c5d056ccdb008e06faa8147c98b0cff5f
robertdeanpantino@gmail.com <robertdeanpantino@gmail.com>
2022-05-10 13:55:43 +0800

    added sanctum package

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']],function() {
+    // Route::get('/products/search/{$name}', [ProductController::class, 'search']);
 });
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
