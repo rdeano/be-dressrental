a4062935a9647ddbc5096da563dc3d175dd1bbc3
robertdeanpantino@gmail.com <robertdeanpantino@gmail.com>
2022-05-12 16:14:14 +0800

    functional login and route guard

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
+
 Route::group(['middleware' => ['auth:sanctum']],function() {
+    Route::post('/logout',[AuthController::class, 'logout']);
     // Route::get('/products/search/{$name}', [ProductController::class, 'search']);
 });
 
